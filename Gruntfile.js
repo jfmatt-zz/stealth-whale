6cf37a85a18d1cadc4206a6fbd4f72023b9ca006
James Matthews <jmatthews@devscapedesigns.com>
2013-10-20 19:54:10 -0400

    Added gh-pages grunt task at `grunt deploy`

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+		copy: {
+			build: {
+				files: [
+					{ expand: true, cwd: 'src', src: ['**'], dest: 'dist/' }
+				]
+			}
+		},
+		uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       }
@@ -16,21 +23,29 @@ module.exports = function(grunt) {
         // options here to override JSHint defaults
       }
     },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint', 'mocha']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-requirejs');
 	grunt.loadNpmTasks('grunt-mocha');
-	
+	grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('test', ['jshint'/*, 'mocha'*/]);
-	grunt.registerTask('build', [/*'requirejs', */'uglify']);
+	grunt.registerTask('build', ['copy:build', /*'requirejs', */'uglify']);
+	grunt.registerTask('deploy', ['test', 'build', 'gh-pages']);
 	
   grunt.registerTask('default', ['test', 'build']);
 
