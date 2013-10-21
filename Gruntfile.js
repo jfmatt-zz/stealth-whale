3b59f2c654d36b364e64c2400344ac46cc2a4e1d
James Matthews <jmatthews@devscapedesigns.com>
2013-10-20 20:03:57 -0400

    formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		copy: {
 			build: {
 				files: [
@@ -10,43 +10,43 @@ module.exports = function(grunt) {
 			}
 		},
 		uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      }
-    },
-    mocha: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-      }
-    },
-    'gh-pages': {
-      options: {
-        base: 'dist'
-      },
-      src: ['**']
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'mocha']
-    }
-  });
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			}
+		},
+		mocha: {
+			files: ['test/**/*.html']
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				// options here to override JSHint defaults
+			}
+		},
+		'gh-pages': {
+			options: {
+				base: 'dist'
+			},
+			src: ['**']
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint', 'mocha']
+		}
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-requirejs');
 	grunt.loadNpmTasks('grunt-mocha');
 	grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('test', ['jshint'/*, 'mocha'*/]);
+	grunt.registerTask('test', ['jshint'/*, 'mocha'*/]);
 	grunt.registerTask('build', ['copy:build', /*'requirejs', */'uglify']);
 	grunt.registerTask('deploy', ['test', 'build', 'gh-pages']);
-	
-  grunt.registerTask('default', ['test', 'build']);
+
+	grunt.registerTask('default', ['test', 'build']);
 
 };
